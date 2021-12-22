6c446586d0ea8275b7574192d6caf3c85312fd41
koichi.ikeda <koichiikeda.0111@gmail.com>
2021-12-22 20:15:18 +0900

    routerの設定とスキーマ設定からdocsを表示まで

--- backend/api/v1/main.py
+++ backend/api/v1/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from api.v1.routers import user,post
+
+app.include_router(user.router)
+app.include_router(post.router)
+
 @app.get("/")
 def read_root():
   return {"Hello":"World"}
